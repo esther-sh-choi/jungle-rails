fcdc9f1524a42fdec8a204b1cecf6ae9d99f2668
So Hyun (Esther) Choi <estherchoi.sh@gmail.com>
2023-03-08 00:27:11 -0500

    Enhance Order detail page

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -44,6 +44,7 @@ class OrdersController < ApplicationController
 
     enhanced_cart.each do |entry|
       product = entry[:product]
+      puts "here", product
       quantity = entry[:quantity]
       order.line_items.new(
         product: product,
@@ -57,3 +58,13 @@ class OrdersController < ApplicationController
   end
 
 end
+
+#<ActiveRecord::Associations::CollectionProxy [#<LineItem id: 1, order_id: 1, product_id: 3, quantity: 1, item_price_cents: 3449, total_price_cents: 3449, created_at: "2023-03-08 02:54:45.930205000 +0000", updated_at: "2023-03-08 02:54:45.930205000 +0000">, #<LineItem id: 2, order_id: 1, product_id: 8, quantity: 1, item_price_cents: 2499, total_price_cents: 2499, created_at: "2023-03-08 02:54:45.950707000 +0000", updated_at: "2023-03-08 02:54:45.950707000 +0000">, #<LineItem id: 3, order_id: 1, product_id: 12, quantity: 1, item_price_cents: 7999, total_price_cents: 7999, created_at: "2023-03-08 02:54:45.955339000 +0000", updated_at: "2023-03-08 02:54:45.955339000 +0000">]>
+
+#   <div>
+# <p><%= @order.line_items[0].name %></p>
+# <p><%= @order.line_items[0].description %></p>
+# <p><%= @order.line_items[0].name %></p>
+# </div>
+
+#<Product id: 3, name: "Lion Grapevine", description: "The Lion Grapevine is an uncommon, modest plant an...", image: "plante_3.jpg", price_cents: 3449, quantity: 4, created_at: "2023-03-07 07:27:53.994297000 +0000", updated_at: "2023-03-07 07:27:53.994297000 +0000", category_id: 1>
