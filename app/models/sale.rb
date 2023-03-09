bd7602b286de902c0bf4ce081a6446d00881a875
So Hyun (Esther) Choi <estherchoi.sh@gmail.com>
2023-03-09 03:09:58 -0500

    Add discount style to website and delete functionality

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,2 +1,18 @@
 class Sale < ApplicationRecord
+  # Creating an active record scope to make it easy to query sales
+  def self.active
+    where("sales.start_date <= ? AND sales.end_date >= ?", Date.current, Date.current)
+  end
+
+  def finished?
+    end_date < Date.current
+  end
+
+  def upcoming?
+    start_date > Date.current
+  end
+
+  def active?
+    !upcoming? && !finished?
+  end
 end
